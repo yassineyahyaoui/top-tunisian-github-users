3a00e8605215440cf230bd8a50ad5039a49e491a
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-09 05:02:44 +0100

    get contributions

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ driver = webdriver.Chrome()
 driver.get("https://github.com/gayanvoice/top-github-users/blob/main/markdown/total_contributions/tunisia.md")
 
 driver.implicitly_wait(60)
-my_list = driver.find_elements(By.CSS_SELECTOR, "table td a img[width='24']")
+avatars_list = driver.find_elements(By.CSS_SELECTOR, "table td a img[width='24']")
 
-for item in my_list:
-    print(item.get_attribute("alt"))
+users_list = []
+
+for avatar in avatars_list:
+    users_list.append(avatar.get_attribute("alt")[10:])
+
+for user in users_list:
+    driver.get("https://github.com/" + user)
+    print(driver.find_element(By.CSS_SELECTOR, ".js-yearly-contributions .position-relative h2.f4.text-normal.mb-2").text[:-31])
 
 
 # driver.implicitly_wait(60)
