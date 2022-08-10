dda116645d56fcfcbaae0a5051d4b5d7738e061f
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-10 07:08:05 +0100

    add readme after table to readme

--- README.txt
+++ README.txt
@@ -5,7 +5,7 @@
 	<img align="right" width="180" src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Tunisia.svg" alt="Tunisia">
 </a>
 
-The `total contributions` by users in Tunisia on `2022 Aug 10 06:24`. This list contains users from `Tunisia` and cities `Tunis` `Sfax` `Sousse` `Kairouan`.
+The `total contributions` by users in Tunisia on `10 Aug 2022 06:56`. This list contains users from `Tunisia` and cities `Tunis` `Sfax` `Sousse` `Kairouan`.
 
 There are `1000 users`  in Tunisia. You need at least `6 followers` to be on this list.
 
@@ -17,4 +17,16 @@ There are `1000 users`  in Tunisia. You need at least `6 followers` to be on thi
 		<th width="350">Name</th>
 		<th width="350">Company</th>
 		<th width="250">Total Contributions</th>
-	</tr>
+	</tr></table>
+
+## 📦 Third party
+
+- [@octokit/graphql](https://www.npmjs.com/package/@octokit/graphql) - Send GraphQL requests to GitHub API.
+- [fs-extra](https://www.npmjs.com/package/fs-extra) - Creating directories and files.
+- [simple-git](https://www.npmjs.com/package/simple-git) - Handling Git commands.
+## 📄 License
+
+- GitHub Action - [gayanvoice/top-github-users-action](https://github.com/gayanvoice/top-github-users-action)
+- Repository - [gayanvoice/top-github-users](https://github.com/gayanvoice/top-github-users)
+- Data in the `./cache` directory - [Open Database License](https://opendatacommons.org/licenses/odbl/1-0/)
+- Code - [MIT](./LICENSE) © [Gayan Kuruppu](https://github.com/gayanvoice)
--- main.py
+++ main.py
@@ -1,42 +1,10 @@
 from datetime import datetime
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-#
-#
-# driver = webdriver.Chrome()
-# driver.get("https://github.com/gayanvoice/top-github-users/blob/main/markdown/total_contributions/tunisia.md")
-#
-# driver.implicitly_wait(60)
-# avatars_list = driver.find_elements(By.CSS_SELECTOR, "table td a img[width='24']")
-#
-# users = []
-# users_list = []
-# for avatar in avatars_list:
-#     users_list.append(avatar.get_attribute("alt")[10:])
-#
-# i = 1
-# for user in users_list:
-#     driver.get("https://github.com/" + user)
-#     driver.implicitly_wait(60)
-#     if driver.find_element(By.CSS_SELECTOR, "span.p-name.vcard-fullname.d-block.overflow-hidden").text != "":
-#         name = driver.find_element(By.CSS_SELECTOR, "span.p-name.vcard-fullname.d-block.overflow-hidden").text
-#     else:
-#         name = "No name"
-#     avatar = driver.find_element(By.CSS_SELECTOR, "img.avatar.avatar-user.width-full.border.color-bg-default").get_attribute("src")
-#     contribution = driver.find_element(By.CSS_SELECTOR, ".js-yearly-contributions .position-relative h2.f4.text-normal.mb-2").text[:-31]
-#     try:
-#         company = driver.find_element(By.CSS_SELECTOR, "li.vcard-detail.pt-1.css-truncate.css-truncate-target.hide-sm.hide-md span.p-org div").text
-#     except:
-#         company = "No company"
-#
-#     print(i, user, name, avatar, company, contribution)
-#     users.append((i, user, name, avatar, company, contribution))
-#     print(users[i - 1])
-#     i = i + 1
-#
-#
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
 now = datetime.now()
-date = now.strftime("%Y %b %d %H:%M")
+date = now.strftime("%d %b %Y %H:%M")
 
 
 readme_head_file = open("readme-head.txt", "r")
@@ -46,6 +14,10 @@ readme_head_file.close()
 readme_before_table_file = open("readme-before-table.txt", "r")
 readme_before_table = readme_before_table_file.read()
 readme_before_table_file.close()
+
+readme_after_table_file = open("readme-after-table.txt", "r")
+readme_after_table = readme_after_table_file.read()
+readme_after_table_file.close()
 
 readme_file = open("README.txt", "w")
 readme_file.write(readme_head)
@@ -58,6 +30,46 @@ readme_file.close()
 readme_file = open("README.txt", "a")
 readme_file.write(readme_before_table)
 readme_file.close()
+
+
+driver = webdriver.Chrome()
+driver.get("https://github.com/gayanvoice/top-github-users/blob/main/markdown/total_contributions/tunisia.md")
+
+driver.implicitly_wait(60)
+avatars_list = driver.find_elements(By.CSS_SELECTOR, "table td a img[width='24']")
+
+users_list = []
+for avatar in avatars_list:
+    users_list.append(avatar.get_attribute("alt")[10:])
+
+readme_file = open("README.txt", "a")
+i = 1
+for user in users_list:
+    driver.get("https://github.com/" + user)
+    driver.implicitly_wait(60)
+    if driver.find_element(By.CSS_SELECTOR, "span.p-name.vcard-fullname.d-block.overflow-hidden").text != "":
+        name = driver.find_element(By.CSS_SELECTOR, "span.p-name.vcard-fullname.d-block.overflow-hidden").text
+    else:
+        name = "No name"
+    avatar = driver.find_element(By.CSS_SELECTOR, "img.avatar.avatar-user.width-full.border.color-bg-default").get_attribute("src")
+    total_contribution = driver.find_element(By.CSS_SELECTOR, ".js-yearly-contributions .position-relative h2.f4.text-normal.mb-2").text[:-31]
+    try:
+        company = driver.find_element(By.CSS_SELECTOR, "li.vcard-detail.pt-1.css-truncate.css-truncate-target.hide-sm.hide-md span.p-org div").text
+    except:
+        company = "No company"
+
+    readme_file.write("<tr>")
+    print(i, user, name, avatar, company, total_contribution)
+    i = i + 1
+readme_file.close()
+
+
+readme_file = open("README.txt", "a")
+readme_file.write(readme_after_table)
+readme_file.close()
+
+
+def write_user(index, username, name, avatar, company, total_contribution)
 
 
 # driver.implicitly_wait(60)
